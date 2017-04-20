24e2dbc16e733dfbf1731d4e13a3f80ba0b83664
yinjianquan <458629451@qq.com>
2017-04-20 09:08:04 +0800

    ..

--- src/components/details.vue
+++ src/components/details.vue
@@ -4,9 +4,9 @@
         <div class="detail-header">
             <div class="detail-header-inside">
                 <div class="goBack">
-                <router-link :to="{name:'orderManage'}"><span class="icon_back"></span><span style="float:left;margin-left:10px;">返回</span></router-link>
+                    <span style="padding-right:5px;"><<</span>返回
                 </div>
-                <div class="inside-detail" id="profit">
+                <div class="inside-detail" id="profit" @click="tabToggle" :class="{'inside-detail-tab':isA =='profit'}">
                     审核小票
                </div>
                 <!--<div class="inside-detail" id="profit" @click="tabToggle" :class="{'inside-detail-tab':isA =='profit'}">
@@ -21,60 +21,50 @@
        <div class="detail-user">
            <div class="detail-tie">用户信息</div>
            <div class="detail-id">
-               <span>用户ID</span><span>{{list.order_number}}</span>
+               <span>用户ID</span><span>12344456767676</span>
            </div>
        </div>
        <!--小票详情-->
        <div class="detail-main">
-           <div class="detail-xiao" style="width:75%;height:300px;float:left;">
+           <div class="detail-xiao" style="width:65%;height:300px;float:left;">
                <div class="detail-tie">小票详情</div>
-               <div style="margin-top:15px;color:#959595;">
-                   <div class="xiao-picture" style="float:left;margin-top:10px;" >
-                       <div class="picture1" style="float:left;"><div><img src="" alt="" style="width:150px;height:190px;border:1px solid #ccc;margin-right:20px;"></div><div style="width:150px;text-align:center;height:30px;line-height:30px;font-size:15px;">完整图</div></div>
-                       <div class="picture2"  style="float:left;"><div><img src="" alt="" style="width:150px;height:190px;border:1px solid #ccc;"></div><div style="width:150px;text-align:center;height:30px;line-height:30px;font-size:15px;">撕裂图</div></div>
+               <div style="margin-top:20px;color:#959595;">
+                   <div class="xiao-picture" style="float:left;" >
+                       <div class="picture1" style="float:left;"><div><img src="" alt="" style="width:140px;height:200px;border:1px solid #ccc;margin-right:15px;"></div><div style="text-align:center;height:30px;line-height:30px;font-size:14px;">完整图</div></div>
+                       <div class="picture2"  style="float:left;"><div><img src="" alt="" style="width:140px;height:200px;border:1px solid #ccc;"></div><div style="text-align:center;height:30px;line-height:30px;font-size:14px;">撕裂图</div></div>
                    </div>
-                   <div style="margin-left:60px;float:left;" class="xiao-text">
+                   <div style="margin-left:50px;float:left;" class="xiao-text">
                        <table>
                            <tr>
                                <td>商户名称</td>
-                               <td>{{list.shop_name}}</td>
+                               <td>海底捞海岸城店</td>
                             </tr>
                             <tr>
                                 <td>小票上传位置</td>
-                                <td>{{list.ticket_position}}<br>N:35`55`435&nbsp;E:45`66`908`</td>
+                                <td>广东省深圳市南山区一路3001号<br>N:35`55`435&nbsp;E:45`66`908`</td>
                             </tr>
                             <tr>
                                 <td>商户地理位置</td>
-                                <td>{{list.shop_position}}<br>N:35`55`435&nbsp;E:45`66`908`<br><span v-bind:class="{'status': !isA}">正常</span></td>
+                                <td>广东省深圳市南山区一路3001号<br>N:35`55`435&nbsp;E:45`66`908`<br>正常</td>
                             </tr>
                             <tr>
                                 <td>小票金额</td>
-                                <td>￥{{list.pay_money}}</td>
+                                <td>￥32.50</td>
                             </tr>
                             <tr>
                                 <td>打印时间</td>
-                                <td>{{list.create_time}}</td>
+                                <td>2017-4-15 17:56:02</td>
                             </tr>
                        </table>
                    </div>
                </div>
            </div>
-           <div class="order-state" style="float:left;border-left:1px dashed #e5e5e5;padding-left:75px;height:300px;">
+           <div class="order-state" style="float:left;border-left:1px dashed #959595;padding-left:170px;height:300px;">
               <div class="detail-tie">订单状态</div>
-               <table class="xiao_state">  
-                   <tr>
-                       <td><img src="../assets/images/noselect.png" alt=""></td>
-                       <td class="xiao_noselect"><div>上传小票</div><div>2017-03-26 12:45:46</div></td>
-                   </tr>
-                   <tr>
-                       <td><img src="../assets/images/selected.png" alt=""></td>
-                       <td v-bind:class="{'xiao_selected':!isA}" style="padding:0px 20px;"><div>审核中</div><div>2017-03-26 12:45:46</div></td>
-                   </tr>
-               </table>
            </div>
        </div>
-       <div class="shen">
-           <div v-on:click="showPop()">审核通过</div>
+       <div class="shen" style="width:100%;">
+           <div>审核通过</div>
            <div>审核失败</div>
        </div>
     </div>
@@ -82,37 +72,20 @@
 </template>
 
 <script>
-//  import dialog from './dialog.vue'
     export default {
         name:'details',
         data () {
-         return {
-            isA:false,
-            list:[]
+            return {
+            isA:false
             }
         },
-        created () {
-            this.getList()
-        },
-         methods: {
-            getList () {
-            this.$ajax({
-            method: 'get',
-            url: 'static/json/ticketDetail.json',
-            data: {
-                userID:"121212",
-                ticketID:"45454545"
+        methods:{
+            tabToggle:function(e){
+            let isA = e.currentTarget.id;
+            this.isA = isA;
             }
-        }).then((res) => {
-            console.log(res);
-            this.list = res.data.data;
-            //   debugger
-            }).catch((err) => {
-              console.log(err)
-            })
         }
-         }
-    
+        
     }
 </script>
 
@@ -121,64 +94,50 @@
         width:1200px;
         background:#fff;
         margin:0 auto;
-        min-height:620px;
+        min-height:600px;
     }
     .detail-header{
         width:100%;
-        height:65px;
+        height:70px;
         border-top: 2px solid #ffad2f;
         margin:0 auto;
-        /*border:1px solid red;*/
+        border:1px solid red;
     }
     .detail-header-inside{
         margin-left: 22px;
-        margin-right: 20px;
-        margin-top: 25px;
+        margin-right: 22px;
+        margin-top: 26px;
         border-bottom: 1px solid #e5e5e5;
-        height: 38px;
+        height: 42px;
         width: 1156px;     
     }
     .detail-header-inside .goBack{
-        height: 14px;
-        line-height: 14px;
-        margin-top: 3px;
-        width: 80px;
+        height: 22px;
+        width: 76px;
         border-right: 1px solid #e5e5e5;
         float: left;
-        color: #ff7b3f;
-        font-size:16px;
+        color: #ffad2f
     }
-    .icon_back{background: url(../assets/images/icon_back.png) no-repeat;width:15px;height:14px;display: block;float:left;}
     .detail-header-inside .inside-detail{
-        margin-left: 27px;
-        height:17;
-        line-height: 17px;
+        margin-left: 40px;
         float: left;
         color: #696969;
-        font-size:17px;
     }
     .detail-header-inside .inside-detail-tab{
         color: #ffad2f;
         border-bottom: 2px solid #ffad2f;
     }
+
     .detail-user{
         padding:30px;
+        border:1px solid red;
     }
-    .detail-tie{font-size:17px;color:#696969;}
-    .detail-user .detail-id{height:40px;line-height:40px;color:#959595;padding-top:10px;font-size:15px;}
+    .detail-tie{font-size:17px;}
+    .detail-user .detail-id{height:40px;line-height:40px;color:#959595;padding-top:10px;}
     .detail-user .detail-id span:last-child{margin-left:25px;}
-    .detail-main{padding:5px 30px;height:300px;}
-    .xiao-text  td{line-height:30px;padding-left:20px;font-size:15px;}
-    .xiao-text tr{vertical-align:top;line-height: 0px;}
-    .xiao-text tr td:first-child{text-align: right;padding-left:40px;}
-    .shen{height:35px;padding:0px 30px;margin-top:20px;}
-    .shen div{float:left;width:100px;height:35px;line-height:35px;background:#ff7b3f;color:#fff;text-align:center;border-radius: 2px;cursor: pointer;}
-    .shen div:last-child{margin-left:25px;}
-    .order-state .xiao_state td{font-size:15px;color:#696969;padding:20px 15px;}
-    .order-state .xiao_state tr td:first-child{padding:10px 0px;}
-    .order-state .xiao_state  .xiao_noselect{line-height: 25px;color:#959595;font-size: 15px;}
-    .order-state .xiao_state  .xiao_selected{color:#ff7b3f;}
-    .order-state .xiao_state{position: relative;}
-    .order-state .xiao_state:before{content: "";width:0px;height:48px;border:1px solid #e5e5e5;position: absolute;top:50px;left:7px;}
-    .status{color:#bae1b5;}
+    .detail-main{padding:10px 30px;height:300px;}
+     .xiao-text td{line-height:30px;padding-left:20px;}
+     .shen{padding:0px 30px;}
+     .shen div{float:left;width:100px;height:35px;line-height:35px;background:#ff7b3f;color:#fff;text-align:center;}
+     .shen div:last-child{margin-left:20px;}
 </style>
--- src/components/userProfit.vue
+++ src/components/userProfit.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <router-link :to="{name:'details'}">查看详情</router-link>
+        <div>
+            <router-link :to="{name:'details'}">查看详情</router-link>
+            <v-data></v-data>
+        </div>
     </div>
-   
 </template>
+<script>
+import vData from './vdata'
+export default {
+  components: {
+    vData
+  }
+}
+</script>
